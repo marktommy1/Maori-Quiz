699d9b5d3ef68f0c931c7b5e7bb0c2d7ac21039a
Mark Tommy <ac121796@avcol.school.nz>
2023-03-15 12:00:56 +1300

    I welcomed the user to the quiz, asked the user for their name and then asked them what level they wanted to play.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name;
+            int level = 0;
+            Console.WriteLine("Welcome to Mark's Maori Quiz");
+            //adding a delay between the text
+            Thread.Sleep(1000);
+            //prompting the user for their name
+            Console.WriteLine("Please enter your name: ");
+            name = Console.ReadLine();
+            //adding a delay between the text
+            Thread.Sleep(1000);
+            //will repeat 
+            while(level != 1 || level !=2 || level !=3)
+            {
+                Console.WriteLine($"Hi {name}, please enter what level you would like to play\n1. Easy\n2. Medium\n3. Hard");
+                level = Convert.ToInt32(Console.ReadLine());
+            }
+            if(level == 1) 
+            {
+                Console.WriteLine("mark is wrong");
+            }
+
         }
     }
 }
