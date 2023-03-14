59ecfbd2d9e2badeac2d68e7160f746a4c8e9b49
Mark Tommy <ac121796@avcol.school.nz>
2023-03-15 11:41:52 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Maori_Quiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
