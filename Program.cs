2bbabbcb5446bae827a3635c543617bbc3d94200
Mark Tommy <ac121796@avcol.school.nz>
2023-04-05 12:30:09 +1200

    The quiz would crash if you got a score more than 6 so I changed the else to an else if and it is now fixed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Maori_Quiz
+﻿using System.Net.Sockets;
+
+namespace Maori_Quiz
 {
     internal class Program
     {
@@ -9,9 +11,9 @@
         }
         static string getname()
         {
-            const int maxchar = 30;
+            const int MAX_CHAR = 30;
             string name;
-            bool validateName = true;
+            bool validate_name = true;
             //prompting the user
             Console.WriteLine("Welcome to Mark's Maori Quiz\n");
             //adding a delay between the text
@@ -20,14 +22,14 @@
             Console.WriteLine("Please enter your name: ");
             name = Console.ReadLine();
             //making sure the user enters a valid name 
-            while(validateName == true)
+            while(validate_name == true)
             {
                 if (name.Length <= 1)
                 {
                     Console.WriteLine("Your name is too short\nPlease enter your name:");
                     name = Console.ReadLine();
                 }
-                else if (name.Length > maxchar)
+                else if (name.Length > MAX_CHAR)
                 {
                     Console.WriteLine("Your name is too long\nPlease enter your name:");
                     name = Console.ReadLine();
@@ -52,8 +54,8 @@
             //declaring variables 
             int score = 0;
             int level = 0;
-            string userInput;
-            string playAgain;
+            string user_input;
+            string play_again;
             bool loop = true;
             //2D array that holds all the questions and answers 
             string[,] questionAnswers = { { "How do you say \"hello\" in Maori?\na. ka kite\nb. noho iho\nc. kia ora", "How do you say \"ocean\" in Maori?\na. moana\nb. awa\nc. katao ", "What does \"haere mai\" mean in Maori?\na. congratulations\nb. go away\nc. come here", "How do you say \"food\" in Maori?\na. waka\nb. ika\nc. kai", "What does \"mahi\" mean in Maori?\na. weclome\nb. work\nc. visitors", "How do you say \"children\" in Maori?\na. tamariki\nb. tamahine\nc. toanga", "What does \"whanau\" mean in Maori?\na. parents\nb. hill\nc. family", "How do you say \"good morning\" in Maori\na. tena koutou\nb. morena\nc. haere ra", "What colour is \"Kowhai\"?\na. blue\nb. red\nc. yellow", "What colour is \"Kikorangi\"?\na. red\nb. blue\nc. orange" }, { "How do you say \"I love you\" in Maori?\na. araoha mai\nb. kei te aroha au ki a koe\nc. tino pai", "What does \"hangi\" mean in Maori?\na. a traditional Maori feast\nb. a type of clothing\nc. a musical instrument", "How do you say \"please\" in Maori?\na. koa\nb. koauau\nc. whakapaoho", "What does \"waiata\" mean in Maori?\na. story\nb. dance\nc. song", "How do you say \"happy birthday\" in Maori?\na. tamariki\nb. kia tau te rangimarie ki a koe\nc. huritaua ki a koe", "What does \"whenua\" mean in Maori?\na. land\nb. sea\nc. sky", "How do you say \"excuse me\" in Maori\na. ka whai whakaro\nb. he aha te take\nc. whakaaetia ahau", "What does \"puku\" mean in Maori?\na. stomach\nb. head\nc. foot", "How do you say \"thank you\" in Maori\na. haere ra\nb. aroha nui\nc. whakawhetai koe", "What does \"mara\" mean in Maori?\na. garden\nb. beach\nc. mountain" }, { "How do you say \"excellence\" in Maori?\na. whakatipu\nb. whakarauora\nc. whakarangatira", "What does \"wairua\" mean in Maori?\na. spirit\nb. body\nc. mind", "How do you say \"sustainability\" in Maori?\na. whakatipu\nb. whakarauora\nc. whakatoputanga", "What does \"tino rangatiratanga\" mean in Maori?\na. self- determination\nb. respect\nc. harmony", "How do you say \"spiritual power\" in Maori?\na. mana tangata\nb. wairua\nc. mauri ora", "What does \"whakawhanaungatanga\" mean in Maori?\na. relationship-building\nb. creativity\nc. persistence", "How do you say \"perfection\" in Maori\na. takakau\nb. whakarewa\nc. whakapau kaha", "What does \"whakapapa\" refer to?\na. land\nb. ancestry\nc. language", "How do you say \"diversity\" in Maori\na. kotahitanga\nb. ahurea rereketanga\nc. whakatairanga", "What does \"manaakitanga\" mean in Maori?\na. hospitality\nb. empowerment\nc. enlightenment" }, { "kia ora", "moana", "come here", "kai", "work", "tamariki", "family", "mornea", "yellow", "blue" }, { "kei te aroha au ki a koe", "a traditional Maori feast", "koa", "song", "huritaua ki a koe", "land", "whakaaetia ahau", "stomach", "whakawhetai koe", "garden" }, { "whakarangatira", "spirit", "whakatoputanga", "self-determination", "wairua", "relationship-building", "takakau", "customary practices", "ahurea rereketanga", "hospitality" }, { "c", "a", "c", "c", "b", "a", "c", "b", "c", "b" }, { "b", "a", "a", "c", "c", "a", "c", "a", "c", "a" }, { "c", "a", "c", "a", "b", "a", "a", "a", "b", "a" } };
@@ -102,15 +104,15 @@
                         //asks the questions 
                         Console.WriteLine($"Question {j + 1}: " + questionAnswers[i, j]);
                         //input from the user 
-                        userInput = Console.ReadLine().ToLower(); 
+                        user_input = Console.ReadLine().ToLower(); 
                         //making sure the user enters valid level
-                        while(!userInput.Equals("a") && !userInput.Equals("b") && !userInput.Equals("c"))
+                        while(!user_input.Equals("a") && !user_input.Equals("b") && !user_input.Equals("c"))
                         {
                             Console.WriteLine("Please enter a valid answer");
-                            userInput = Console.ReadLine().ToLower();
+                            user_input = Console.ReadLine().ToLower();
                         }
                         //got the answer correct
-                        if (userInput.Equals(questionAnswers[level + 5, j]))
+                        if (user_input.Equals(questionAnswers[level + 5, j]))
                         {
                             score += 1;
                             Console.WriteLine($"Good job {name} you got it right!\nYou're current score is: {score}/{j+1}");
@@ -121,15 +123,15 @@
                         {
                             //giving the user a second chance 
                             Console.WriteLine("Try again");
-                            userInput = Console.ReadLine().ToLower();
+                            user_input = Console.ReadLine().ToLower();
                             //making sure the user enters valid input
-                            while (!userInput.Equals("a") && !userInput.Equals("b") && !userInput.Equals("c"))
+                            while (!user_input.Equals("a") && !user_input.Equals("b") && !user_input.Equals("c"))
                             {
                                 Console.WriteLine("Please enter a valid answer");
-                                userInput = Console.ReadLine().ToLower();
+                                user_input = Console.ReadLine().ToLower();
                             }
                             //got it correct on second try
-                            if (userInput.Equals(questionAnswers[level + 5, j]))
+                            if (user_input.Equals(questionAnswers[level + 5, j]))
                             {
                                 Console.WriteLine($"You got it on your second try. You don't get any points though.\nYour current score is: {score}/{j+1}");
                                 Thread.Sleep(2000);
@@ -154,20 +156,20 @@
                 {
                     Console.WriteLine($"Not to bad {name} your score is {score}/10");
                 }
-                else
+                else if (score <=10)
                 {
                     Console.WriteLine($"Great job {name}! Your score is {score}/10");
                 }   
                 //asking the user if they want to play again
                 Console.WriteLine("Would you like to play again?(y / n)");
-                playAgain = Console.ReadLine().ToLower();
-                //making sure the user enters valid input
-                while (!playAgain.Equals("y") && !playAgain.Equals("n"))
+                play_again = Console.ReadLine().ToLower();
+                //making sure the user enters valid input       
+                while (!play_again.Equals("y") && !play_again.Equals("n"))
                 {
                     Console.WriteLine("Please enter a valid input");
-                    playAgain = Console.ReadLine().ToLower();
+                    play_again = Console.ReadLine().ToLower();
                 }
-                if(playAgain.Equals("n"))
+                if(play_again.Equals("n"))
                 {
                     Console.WriteLine("Thanks for playing! See you next time :)");
                     break;
