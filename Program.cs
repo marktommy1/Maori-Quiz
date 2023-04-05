16064f86b91bd2f422c75b6195126818ea895eea
Mark Tommy <ac121796@avcol.school.nz>
2023-04-05 12:04:39 +1200

    I added comments to make the code easier the understand. I changed the variable name of the constant to all caps. I also changed the variable names using underscores rather than camel case

--- Program.cs
+++ Program.cs
@@ -49,6 +49,7 @@
         }
         static void play(string name)
         {
+            //declaring variables 
             int score = 0;
             int level = 0;
             string userInput;
@@ -56,8 +57,10 @@
             bool loop = true;
             //2D array that holds all the questions and answers 
             string[,] questionAnswers = { { "How do you say \"hello\" in Maori?\na. ka kite\nb. noho iho\nc. kia ora", "How do you say \"ocean\" in Maori?\na. moana\nb. awa\nc. katao ", "What does \"haere mai\" mean in Maori?\na. congratulations\nb. go away\nc. come here", "How do you say \"food\" in Maori?\na. waka\nb. ika\nc. kai", "What does \"mahi\" mean in Maori?\na. weclome\nb. work\nc. visitors", "How do you say \"children\" in Maori?\na. tamariki\nb. tamahine\nc. toanga", "What does \"whanau\" mean in Maori?\na. parents\nb. hill\nc. family", "How do you say \"good morning\" in Maori\na. tena koutou\nb. morena\nc. haere ra", "What colour is \"Kowhai\"?\na. blue\nb. red\nc. yellow", "What colour is \"Kikorangi\"?\na. red\nb. blue\nc. orange" }, { "How do you say \"I love you\" in Maori?\na. araoha mai\nb. kei te aroha au ki a koe\nc. tino pai", "What does \"hangi\" mean in Maori?\na. a traditional Maori feast\nb. a type of clothing\nc. a musical instrument", "How do you say \"please\" in Maori?\na. koa\nb. koauau\nc. whakapaoho", "What does \"waiata\" mean in Maori?\na. story\nb. dance\nc. song", "How do you say \"happy birthday\" in Maori?\na. tamariki\nb. kia tau te rangimarie ki a koe\nc. huritaua ki a koe", "What does \"whenua\" mean in Maori?\na. land\nb. sea\nc. sky", "How do you say \"excuse me\" in Maori\na. ka whai whakaro\nb. he aha te take\nc. whakaaetia ahau", "What does \"puku\" mean in Maori?\na. stomach\nb. head\nc. foot", "How do you say \"thank you\" in Maori\na. haere ra\nb. aroha nui\nc. whakawhetai koe", "What does \"mara\" mean in Maori?\na. garden\nb. beach\nc. mountain" }, { "How do you say \"excellence\" in Maori?\na. whakatipu\nb. whakarauora\nc. whakarangatira", "What does \"wairua\" mean in Maori?\na. spirit\nb. body\nc. mind", "How do you say \"sustainability\" in Maori?\na. whakatipu\nb. whakarauora\nc. whakatoputanga", "What does \"tino rangatiratanga\" mean in Maori?\na. self- determination\nb. respect\nc. harmony", "How do you say \"spiritual power\" in Maori?\na. mana tangata\nb. wairua\nc. mauri ora", "What does \"whakawhanaungatanga\" mean in Maori?\na. relationship-building\nb. creativity\nc. persistence", "How do you say \"perfection\" in Maori\na. takakau\nb. whakarewa\nc. whakapau kaha", "What does \"whakapapa\" refer to?\na. land\nb. ancestry\nc. language", "How do you say \"diversity\" in Maori\na. kotahitanga\nb. ahurea rereketanga\nc. whakatairanga", "What does \"manaakitanga\" mean in Maori?\na. hospitality\nb. empowerment\nc. enlightenment" }, { "kia ora", "moana", "come here", "kai", "work", "tamariki", "family", "mornea", "yellow", "blue" }, { "kei te aroha au ki a koe", "a traditional Maori feast", "koa", "song", "huritaua ki a koe", "land", "whakaaetia ahau", "stomach", "whakawhetai koe", "garden" }, { "whakarangatira", "spirit", "whakatoputanga", "self-determination", "wairua", "relationship-building", "takakau", "customary practices", "ahurea rereketanga", "hospitality" }, { "c", "a", "c", "c", "b", "a", "c", "b", "c", "b" }, { "b", "a", "a", "c", "c", "a", "c", "a", "c", "a" }, { "c", "a", "c", "a", "b", "a", "a", "a", "b", "a" } };
+            //used to repeat the quiz if the user wants
             while (loop == true)
             {
+                //resets the users score and level
                 score = 0;
                 level = 0;
                 Console.WriteLine($"\nHi {name}, please enter what level you would like to play\n1. Easy\n2. Medium\n3. Hard");
@@ -68,6 +71,7 @@
                     {
                         level = Convert.ToInt32(Console.ReadLine());
                     }
+                    //catching the input
                     catch (FormatException)
                     {
                         Console.WriteLine("Please enter a valid level: ");
@@ -94,17 +98,18 @@
                 for (int i = 0; i < level; i++)
                 {
                     for (int j = 0; j < questionAnswers.GetLength(1); j++)
-                    {
-
+                    {   
+                        //asks the questions 
                         Console.WriteLine($"Question {j + 1}: " + questionAnswers[i, j]);
-                        userInput = Console.ReadLine().ToLower();
-                        //making sure the user inputs a valid level
+                        //input from the user 
+                        userInput = Console.ReadLine().ToLower(); 
+                        //making sure the user enters valid level
                         while(!userInput.Equals("a") && !userInput.Equals("b") && !userInput.Equals("c"))
                         {
                             Console.WriteLine("Please enter a valid answer");
                             userInput = Console.ReadLine().ToLower();
                         }
-                        
+                        //got the answer correct
                         if (userInput.Equals(questionAnswers[level + 5, j]))
                         {
                             score += 1;
@@ -117,17 +122,20 @@
                             //giving the user a second chance 
                             Console.WriteLine("Try again");
                             userInput = Console.ReadLine().ToLower();
+                            //making sure the user enters valid input
                             while (!userInput.Equals("a") && !userInput.Equals("b") && !userInput.Equals("c"))
                             {
                                 Console.WriteLine("Please enter a valid answer");
                                 userInput = Console.ReadLine().ToLower();
                             }
+                            //got it correct on second try
                             if (userInput.Equals(questionAnswers[level + 5, j]))
                             {
                                 Console.WriteLine($"You got it on your second try. You don't get any points though.\nYour current score is: {score}/{j+1}");
                                 Thread.Sleep(2000);
                                 Console.Clear();
                             }
+                            //got the answer incorrect
                             else
                             {
                                 Console.WriteLine($"Unlucky, the correct answer was {questionAnswers[level + 2, j]}\nYour current score is: {score}/{j+1}");
@@ -137,6 +145,7 @@
                         }
                     }
                 }//end of for loops
+                //displaying different things depending on the users score
                 if (score <= 3)
                 {
                     Console.WriteLine($"Hard luck {name} your score is {score}/10");
@@ -149,9 +158,11 @@
                 {
                     Console.WriteLine($"Great job {name}! Your score is {score}/10");
                 }   
+                //asking the user if they want to play again
                 Console.WriteLine("Would you like to play again?(y / n)");
                 playAgain = Console.ReadLine().ToLower();
-                while(!playAgain.Equals("n") && !playAgain.Equals("n"))
+                //making sure the user enters valid input
+                while (!playAgain.Equals("y") && !playAgain.Equals("n"))
                 {
                     Console.WriteLine("Please enter a valid input");
                     playAgain = Console.ReadLine().ToLower();
